996d46ff6f85d07db9e13957a919b5170ba085ca
Clifton <clifton314@gmail.com>
2023-09-23 01:10:24 -0400

    Finished up logic for toast notification controll

--- client/src/components/toastCont.js
+++ client/src/components/toastCont.js
@@ -1,16 +1,41 @@
-import React, {useState, useRef, useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Toast from "./toastNoti";
 
+export default function ToastContainer({ data }) {
+  //const listRef = useRef(null)
+  const [toasts, setToasts] = useState([]);
 
-export default function ToastContainer({ data, removeToast }) {
-    const listRef = useRef(null)
+  function removeToast(id) {
+    setToasts((prev) => prev.filter((toast) => toast.id !== id));
+  }
 
-    return (
-        data.length > 0 && (
-        <div className="toast-list">
-            {data.map(toast => (
-                <Toast key={toast.id} message={toast.message} type={toast.type} onClose={() => removeToast(toast.id)} />
-            ))}
-        </div>)
+  function removeAll() {
+    setToasts([]);
+  }
+
+  if (data) {
+    //data must be an array of objects
+    for (const toast in data) {
+      setToasts((prev) => [...prev, toast]);
+      setTimeout(() => removeToast(toast.id), 5000);
+    }
+  }
+
+  //add auto delete, useEffect?
+
+  return (
+    toasts.length > 0 && (
+      <div className="toast-list">
+        {toasts.map((toast) => (
+          <Toast
+            key={toast.id}
+            message={toast.message}
+            type={toast.type}
+            onClose={() => removeToast(toast.id)}
+          />
+        ))}
+        <button onClick={removeAll}>Close All</button>
+      </div>
     )
+  );
 }
